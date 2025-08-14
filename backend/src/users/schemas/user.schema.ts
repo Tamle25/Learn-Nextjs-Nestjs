2471c5b5871c9aae5874a8fe49f958c5b99715bc
Tamle25 <tamlcb1k22ts4@gmail.com>
2025-08-14 09:21:45 +0700

    create database

--- backend/src/users/schemas/user.schema.ts
+++ backend/src/users/schemas/user.schema.ts
@@ -1,1 +1,15 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop()
+  name: string;
+
+  @Prop()
+  age: number;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
